06b9c908f9356476836572b5d7d3bf06b25b02c2
Deondre English <deondreenglish@Deondres-iMac.local>
2021-12-17 15:25:14 -0500

    Docs Website setup both Frontend and Backend Have been started.

--- docs/website_docs/src/App.vue
+++ docs/website_docs/src/App.vue
@@ -1,17 +1,74 @@
 <script setup lang="ts">
+  import './lib/css/Pages/Homepage.scss';
+  import Navbar from './Components/Navbar.vue';
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <div>
+    <nav>
+      <Navbar />
+    </nav>
+    <div class="container">
+      <div>
+        <img src="./assets/Logo.png"/>
+      </div>
+      <div class="description">
+        Bots Made Easy.
+      </div>
+      <div class="buttons">
+        <button><a href="#">Get Started</a></button>
+        <button><a href="#">Learn More</a></button>
+      </div>
+      <hr />
+      <div class="grid-container">
+        <div class="grid-item">
+          <span>🛠 Rich Features</span>
+          <p>
+            Support for generating in multiple languages such as: <br /> 
+            Typescript, Javascript, and Python
+          </p>
+        </div>
+        <div class="grid-item">
+          <span>🛠 Rich Features</span>
+          <p>
+            Support for generating in multiple languages such as: <br /> 
+            Typescript, Javascript, and Python
+          </p>
+        </div>
+        <div class="grid-item">
+          <span>🛠 Rich Features</span>
+          <p>
+            Support for generating in multiple languages such as: <br /> 
+            Typescript, Javascript, and Python
+          </p>
+        </div>
+        <div class="grid-item" style="margin-top: 10px;">
+          <span>⚡️ Super Fast</span>
+          <p>
+            Nomatter on if you are using the web based tool <br />
+            or, the CLI both are very fast</p>
+        </div>
+        <div class="grid-item" style="margin-top: 10px;">
+          <span>🔧 Univesal Features</span>
+          <p>Features availbile on all platforms.</p>
+        </div>
+        <div class="grid-item" style="margin-top: 10px;">
+          <span>📚 Fully Typed APIs</span>
+          <p>Flexible programmatic APIs with fully Typed features</p>
+        </div>
+      </div>
+      <hr />
+      <div class="sponsers">
+        <h3>Sponsers</h3>
+        <p>
+          Become a sponser on Github here: <br />
+          <a href="https://github.com/BotMaker-Ecobot/Ecobot-Source">Ecobot-Source</a></p>
+      </div>
+      <hr />
+      <div class="copyright">
+        <p>MIT Licensed | Copyright © 2021-Present</p>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
